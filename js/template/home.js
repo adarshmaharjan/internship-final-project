dd5dae64a03ab5bad277378d1e93b810a8f35370
adarsh <adarsh986030@gmail.com>
2022-01-01 15:14:26 +0545

    [added] added templates for diffrent pages

---
+++ js/template/home.js
@@ -0,0 +1,8 @@
+const homePage = `	<div class="wrapper">
+<div id="banner">
+  <h1>Welcome to Resume Creator</h1>
+  <button class='btn resume-btn'>Create Resume</button>
+</div>
+</div>`
+
+export default homePage
